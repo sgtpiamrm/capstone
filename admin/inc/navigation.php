6f7b56b328dcd38c8d0808ca502153ca1ca08d54
nicole <nicolesamson25@gmail.com>
2024-11-12 01:09:45 +0800

    Update the bg-white

--- admin/inc/navigation.php
+++ admin/inc/navigation.php
@@ -4,7 +4,7 @@
   <!-- Brand Logo -->
   <a href="<?php echo base_url ?>admin" class="brand-link bg-transparent text-sm shadow-sm">
     <img src="<?php echo validate_image($_settings->info('logo')) ?>" alt="Store Logo"
-      class="brand-image img-circle elevation-3 bg-black"
+      class="brand-image img-circle elevation-3 bg-white"
       style="width: 1.8rem;height: 1.8rem;max-height: unset;object-fit:scale-down;object-position:center center">
     <span class="brand-text font-weight-light"><?php echo $_settings->info('short_name') ?></span>
   </a>
